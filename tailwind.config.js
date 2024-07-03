d4bb51b63d3467eb64e70b0d8139b3f34c947384
k-ravi24 <k.ravi19777@gmail.com>
2024-07-03 09:40:41 +0530

    added: screens || fontFamily in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "portrait-mobile": {
+          raw: "(max-width: 640px) and (orientation: portrait)",
+        }, // Portrait mode for mobile
+        "landscape-mobile": {
+          raw: "(max-width: 640px) and (orientation: landscape)",
+        }, // Landscape mode for mobile
+        tab: "768px", // Tablet size
+        "small-laptop": "1024px", // Small laptop
+        "mid-laptop": "1280px", // Mid-sized laptop
+        laptop: "1440px", // Standard laptop
+        desktop: "1920px", // Desktop
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
