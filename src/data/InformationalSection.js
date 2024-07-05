42ba755d17bbfac757e4374ebaa4307aae001622
k-ravi24 <k.ravi19777@gmail.com>
2024-07-05 09:48:50 +0530

    Create InformationalSection.js

---
+++ src/data/InformationalSection.js
@@ -0,0 +1,27 @@
+import noanimalproduct from "../../Images/noanimalproduct.jpg";
+import whyvegan from "../../Images/whyvegan.jpg";
+
+const information = [
+  {
+    thumbnail: {
+      url: whyvegan,
+    },
+    info: {
+      title: "Why Go Vegan",
+      subtitle: "For Health, Compassion and the Planet",
+      buttonText: "Explore",
+    },
+  },
+  {
+    thumbnail: {
+      url: noanimalproduct,
+    },
+    info: {
+      title: "Dairy: Behind the Scene",
+      subtitle: "The Journey from Farm to Table",
+      buttonText: "Explore",
+    },
+  },
+];
+
+export default information;
