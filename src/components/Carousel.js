434023f0b0938ec2976079417b1307c9e0ad6f64
k-ravi24 <k.ravi19777@gmail.com>
2024-07-09 09:45:22 +0530

    created: Carousel || removed horizontal scroller visibility

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,168 +1,42 @@
 import { ArrowBack, ArrowForward } from "@mui/icons-material";
-import { useEffect, useRef, useState } from "react";
+import RestroCard from "./RestroCard";
 
-const Carousel = ({ spaceBetween }) => {
-  const carouselContainer = useRef(null);
-  const carousel = useRef(null);
-  const card = useRef(null);
-  const [carouselReference, setCarouselReference] = useState({
-    carouselContainerWidth: 0,
-    carouselWidth: 0,
-    cardWidth: 0,
-  });
-
-  useEffect(() => {
-    setCarouselReference({
-      carouselContainerWidth: carouselContainer.current.offsetWidth,
-      carouselWidth: carousel.current.offsetWidth,
-      cardWidth: carousel.current.offsetWidth,
-    });
-  }, [carouselContainer, carousel, card]);
-
-  const handleRightArrowClick = () => {
-    carouselContainer.current.scrollLeft += carouselReference.cardWidth;
-  };
-
-  const handleLeftArrowClick = () => {
-    carouselContainer.current.scrollLeft -= carouselReference.cardWidth;
-  };
+const Carousel = ({ restaurants }) => {
   return (
-    <div className="w-full h-full relative mid-laptop:px-12">
-      <div className="overflow-hidden" ref={carouselContainer}>
-        <div
-          className={`w-fit flex overflow-x-scroll`}
-          style={{ gap: `${spaceBetween}px` }}
-          ref={carousel}
-        >
-          <div
-            style={{
-              width: "20rem",
-              height: "15rem",
-              border: "1px solid black",
-              borderRadius: "0.375rem",
-            }}
-            ref={card}
-          >
-            This is Card
-          </div>
-
-          <div
-            style={{
-              width: "20rem",
-              height: "15rem",
-              border: "1px solid black",
-              borderRadius: "0.375rem",
-            }}
-          >
-            This is Card
-          </div>
-
-          <div
-            style={{
-              width: "20rem",
-              height: "15rem",
-              border: "1px solid black",
-              borderRadius: "0.375rem",
-            }}
-          >
-            This is Card
-          </div>
-
-          <div
-            style={{
-              width: "20rem",
-              height: "15rem",
-              border: "1px solid black",
-              borderRadius: "0.375rem",
-            }}
-          >
-            This is Card
-          </div>
-
-          <div
-            style={{
-              width: "20rem",
-              height: "15rem",
-              border: "1px solid black",
-              borderRadius: "0.375rem",
-            }}
-          >
-            This is Card
-          </div>
-
-          <div
-            style={{
-              width: "20rem",
-              height: "15rem",
-              border: "1px solid black",
-              borderRadius: "0.375rem",
-            }}
-          >
-            This is Card
-          </div>
-
-          <div
-            style={{
-              width: "20rem",
-              height: "15rem",
-              border: "1px solid black",
-              borderRadius: "0.375rem",
-            }}
-          >
-            This is Card
-          </div>
-
-          <div
-            style={{
-              width: "20rem",
-              height: "15rem",
-              border: "1px solid black",
-              borderRadius: "0.375rem",
-            }}
-          >
-            This is Card
-          </div>
-
-          <div
-            style={{
-              width: "20rem",
-              height: "15rem",
-              border: "1px solid black",
-              borderRadius: "0.375rem",
-            }}
-          >
-            This is Card
-          </div>
-
-          <div
-            style={{
-              width: "20rem",
-              height: "15rem",
-              border: "1px solid black",
-              borderRadius: "0.375rem",
-            }}
-          >
-            This is Card
+    <>
+      <div className="w-full overflow-hidden px-12 relative">
+        <div className="w-full overflow-x-scroll hide-scrollbar">
+          <div className="w-fit">
+            <div className={`w-fit flex`}>
+              {restaurants.map((item) => (
+                <div
+                  key={item.id}
+                  className="small-laptop:mr-2 mid-laptop:mr-4"
+                >
+                  <RestroCard
+                    name={item.name}
+                    address={item.address}
+                    cuisines={item.cuisines}
+                    type={item.type}
+                    rating={item.rating}
+                    thumbnail={item.thumbnail}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* left arrow */}
-      <div
-        className="absolute hidden top-1/2 left-0 rounded-full bg-slate-900 text-white mid-laptop:w-8 mid-laptop:h-8 mid-laptop:flex items-center justify-center"
-        onClick={handleLeftArrowClick}
-      >
-        <ArrowBack fontSize="sm" />
+        {/* left arrow */}
+        <button className="absolute top-1/2 left-0 small-laptop:w-8 small-laptop:h-8 rounded-full text-white hidden small-laptop:flex items-center justify-center bg-slate-800">
+          <ArrowBack fontSize="sm" />
+        </button>
+        {/* right arrow */}
+        <button className="absolute top-1/2 right-0 small-laptop:flex items-center justify-center bg-slate-800 text-white hidden small-laptop:w-8 small-laptop:h-8 rounded-full">
+          <ArrowForward fontSize="sm" />
+        </button>
       </div>
-
-      {/* right arrow */}
-      <div
-        className="absolute top-1/2 right-0 hidden mid-laptop:flex items-center justify-center mid-laptop:w-8 mid-laptop:h-8 bg-slate-800 text-white rounded-full"
-        onClick={handleRightArrowClick}
-      >
-        <ArrowForward fontSize="sm" />
-      </div>
-    </div>
+    </>
   );
 };
 
